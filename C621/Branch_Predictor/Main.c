698543a0edec76938b09296506867bc07e57d81d
shihao-song <shihao.song@drexel.edu>
2019-09-01 10:53:10 -0400

    Start working on Branch Predictor

---
+++ C621/Branch_Predictor/Main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{	
+    if (argc != 2)
+    {
+        printf("Usage: %s %s\n", argv[0], "<trace-file>");
+
+        return 0;
+    }
+}
