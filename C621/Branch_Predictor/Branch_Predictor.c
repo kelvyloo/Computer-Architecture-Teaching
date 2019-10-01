b06bfa96098b16054f6010b8c78d93cfdd10b5ba
shihao-song <shihao.song@drexel.edu>
2019-09-30 20:54:45 -0400

    Fixed an issue

--- C621/Branch_Predictor/Branch_Predictor.c
+++ C621/Branch_Predictor/Branch_Predictor.c
@@ -219,6 +219,8 @@ bool predict(Branch_Predictor *branch_predictor, Instruction *instr)
 
     // Step six, update global history register
     branch_predictor->global_history = branch_predictor->global_history << 1 | instr->taken;
+    local_history_table[local_history_table_index] =
+            local_history_table[local_history_table_index] << 1 | instr->taken;
     // exit(0);
     //
     return prediction_correct;
